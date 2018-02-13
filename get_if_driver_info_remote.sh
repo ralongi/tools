e6b38288c6385e2e75dbe5747f9b4f483affcf67
Rick Alongi <ralongi@ralongi.bos.csb>
2018-02-13 10:33:14 -0500

    get_if_driver_info_remote.sh: Added logic to catch error conditions.

--- get_if_driver_info_remote.sh
+++ get_if_driver_info_remote.sh
@@ -3,9 +3,22 @@
 # Script to log into target host and gather iface and driver info
 
 target=$1
+password=$2
 
-if [[ $# -lt 1 ]]; then
-	echo "You must specify a target host ($0 <target_host>)."
+if [[ $# -lt 2 ]]; then
+	echo "You must specify a target host and root password($0 <target_host> <root pw>)."
+	exit
+fi
+
+rhel_version=$(cut -f1 -d. /etc/redhat-release | sed 's/[^0-9]//g')
+if [[ $rhel_version -eq 7 ]]; then
+	sshpass_rpm="https://rpmfind.net/linux/epel/7/x86_64/Packages/s/sshpass-1.06-1.el7.x86_64.rpm"
+elif [[ $rhel_version -eq 7 ]]; then
+	sshpass_rpm="https://rpmfind.net/linux/epel/6/x86_64/Packages/s/sshpass-1.06-1.el6.x86_64.rpm"
+fi
+
+if [[ ! $(which sshpass) ]]; then
+	echo "sshpass must be installed for this tool to work.  You may find the sshpass package for your system here:  $sshpass_rpm"
 	exit
 fi
 
@@ -15,6 +28,5 @@ if [[ $? -ne 0 ]]; then
 	exit
 fi
 
-#cat /home/inf_ralongi/scripts/new_get_if_driver_info.sh | ssh root@$target 'bash -'
-cat ./get_if_driver_info.sh | ssh root@$target 'bash -'
+cat ./get_if_driver_info.sh | sshpass -p $password ssh -o "StrictHostKeyChecking no" root@$target 'bash -'
 
