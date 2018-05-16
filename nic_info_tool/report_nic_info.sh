2fc85f867c0f2aafd9f1c7157e7ee4c38085a80f
Rick Alongi <ralongi@ralongi.bos.csb>
2018-05-16 18:39:00 -0400

    ../report_nic_info.sh: Added vnet and tun to list of iface types to skip.

--- nic_info_tool/report_nic_info.sh
+++ nic_info_tool/report_nic_info.sh
@@ -8,7 +8,7 @@ echo "NIC and Interface list for $(hostname):" >> /home/nic_info.txt
 echo -e "Note that only physical interfaces are listed\n" >> /home/nic_info.txt
 echo "---------------------------------------------------------------------" >> /home/nic_info.txt
 
-for i in $(ls /sys/class/net | egrep -v 'lo|ovs|vir'); do
+for i in $(ls /sys/class/net | egrep -v 'lo|ovs|vir|vnet|tun'); do
 	driver=$(ethtool -i $i | grep driver | awk '{print $NF}')
 	pci_slot=$(ethtool -i $i | grep bus | awk -F ":" '{print $3":"$4$5}')
 	nic_name=$(lspci -v | grep $pci_slot | awk -F ":" '{print $NF}')
