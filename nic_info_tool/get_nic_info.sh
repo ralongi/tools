82800c6177041858d07505eccf4b01dbed0787e9
Rick Alongi <ralongi@redhat.com>
2020-02-14 08:01:18 -0500

    get_nic_info.sh: Added Link detetced and port speed values.

--- nic_info_tool/get_nic_info.sh
+++ nic_info_tool/get_nic_info.sh
@@ -15,10 +15,15 @@ for i in $(ls /sys/class/net | egrep -v "lo|ovs|vir|vnet|tun|$mgmt_iface"); do
 	driver=$(ethtool -i $i | grep driver | awk '{print $NF}')
 	pci_slot=$(ethtool -i $i | grep bus | awk -F ":" '{print $3":"$4$5}')
 	nic_name=$(lspci -v | grep $pci_slot | awk -F ":" '{print $NF}')
+	speed=$(ethtool $i | grep Speed | awk '{print $2}' | tr -d '[a-z A-Z /]')
+	if [[ $(echo $speed | grep '!') ]]; then speed="Unknown"; fi
+	link_detected=$(ethtool $i | grep 'Link detected' | awk '{print $NF}')
 	echo -e "Interface: $i\n" >> /home/nic_info.txt
 	echo "    Driver: $driver" >> /home/nic_info.txt
 	echo "    Card: $nic_name" >> /home/nic_info.txt
 	echo "    PCI Slot: $pci_slot" >> /home/nic_info.txt
+	echo "    Speed: $speed Mbps" >> /home/nic_info.txt
+	echo "    Link detected: $link_detected" >> /home/nic_info.txt
 	echo "---------------------------------------------------------------------" >> /home/nic_info.txt
 done
 
