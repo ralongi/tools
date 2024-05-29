3c5d4770046f84e1a289c0a981cc5d7be094a7db
Rick Alongi <ralongi@ralongi.bos.csb>
2024-05-29 00:00:01 -0400

    Changes

--- ovs_testing/exec_my_ovs_tests.sh
+++ ovs_testing/exec_my_ovs_tests.sh
@@ -141,13 +141,13 @@ get_latest_driverctl()
 get_latest_driverctl
 
 if [[ -z $RPM_DRIVERCTL ]]; then
-	export RPM_DRIVERCTL=$DRIVERCTL_RHEL8
+	export RPM_DRIVERCTL=$DRIVERCTL_RHEL9
 fi
 if [[ -z $RPM_OVS_TCPDUMP_PYTHON ]]; then
-	export RPM_OVS_TCPDUMP_PYTHON=$OVS310_PYTHON_24D_RHEL8
+	export RPM_OVS_TCPDUMP_PYTHON=$OVS217_PYTHON_24D_RHEL9
 fi
 if [[ -z $RPM_OVS_TCPDUMP_TEST ]]; then
-	export RPM_OVS_TCPDUMP_TEST=$OVS310_TCPDUMP_24D_RHEL8
+	export RPM_OVS_TCPDUMP_TEST=$OVS217_TCPDUMP_24D_RHEL9
 fi
 
 # RHEL composes
@@ -250,14 +250,14 @@ export SRC_NETPERF="http://netqe-infra01.knqe.eng.rdu2.dc.redhat.com/share/tools
 
 # VM image names
 if [[ -z $VM_IMAGE ]]; then
-	export VM_IMAGE="rhel8.6.qcow2"
+	export VM_IMAGE="rhel9.2.qcow2"
 else
 	export VM_IMAGE=$VM_IMAGE
 fi
 
 # OVS packages
 if [[ -z $RPM_OVS ]]; then
-	export RPM_OVS=$OVS310_24D_RHEL8
+	export RPM_OVS=$OVS217_24D_RHEL9
 else
 	export RPM_OVS=$RPM_OVS
 fi
@@ -279,7 +279,7 @@ fi
 
 # SELinux packages
 if [[ -z $RPM_OVS_SELINUX_EXTRA_POLICY ]]; then
-	export RPM_OVS_SELINUX_EXTRA_POLICY=$OVS_SELINUX_24D_RHEL8
+	export RPM_OVS_SELINUX_EXTRA_POLICY=$OVS_SELINUX_24D_RHEL9
 else
 	export RPM_OVS_SELINUX_EXTRA_POLICY=$RPM_OVS_SELINUX_EXTRA_POLICY
 fi
@@ -298,7 +298,7 @@ fi
 #export QEMU_KVM_RHEV_RHEL7=http://download.devel.redhat.com/brewroot/packages/qemu-kvm-rhev/2.12.0/48.el7_9.2/x86_64/qemu-kvm-rhev-2.12.0-48.el7_9.2.x86_64.rpm
 
 # OVN packages
-export RPM_OVN=$OVN310_24D_RHEL8 
+export RPM_OVN=$OVN217_24D_RHEL9 
 
 export BONDING_TESTS="ovs_test_bond_active_backup ovs_test_bond_set_active_slave ovs_test_bond_lacp_active ovs_test_bond_lacp_passive ovs_test_bond_balance_slb ovs_test_bond_balance_tcp"
 
@@ -318,7 +318,7 @@ pushd /home/ralongi/github/tools/ovs_testing
 #./exec_sanity_check.sh
 #./exec_ovs_qos.sh
 #./exec_mcast_snoop.sh
-./exec_power_cycle_crash.sh
+#./exec_power_cycle_crash.sh
 #./exec_forward_bpdu.sh
 #./exec_of_rules.sh
 
@@ -353,7 +353,7 @@ pushd /home/ralongi/github/tools/ovs_testing
 ##./exec_topo.sh 6820c ovs_env=ovs-dpdk
 
 #./exec_endurance.sh cx5
-#./exec_perf_ci.sh cx5
+./exec_perf_ci.sh cx5
 #./exec_endurance.sh cx6dx
 #./exec_perf_ci.sh cx6dx
 #./exec_endurance.sh cx6lx
