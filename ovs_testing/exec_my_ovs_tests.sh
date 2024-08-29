636092520f5def2450aa23aebbba96a500099b5c
Rick Alongi <ralongi@ralongi-thinkpadp16vgen1.rmtusma.csb>
2024-08-29 00:00:00 -0400

    Changes

--- ovs_testing/exec_my_ovs_tests.sh
+++ ovs_testing/exec_my_ovs_tests.sh
@@ -144,10 +144,10 @@ if [[ -z $RPM_DRIVERCTL ]]; then
 	export RPM_DRIVERCTL=$DRIVERCTL_RHEL9
 fi
 if [[ -z $RPM_OVS_TCPDUMP_PYTHON ]]; then
-	export RPM_OVS_TCPDUMP_PYTHON=$OVS340_PYTHON_24F_RHEL9
+	export RPM_OVS_TCPDUMP_PYTHON=$OVS330_PYTHON_24F_RHEL9
 fi
 if [[ -z $RPM_OVS_TCPDUMP_TEST ]]; then
-	export RPM_OVS_TCPDUMP_TEST=$OVS340_TCPDUMP_24F_RHEL9
+	export RPM_OVS_TCPDUMP_TEST=$OVS330_TCPDUMP_24F_RHEL9
 fi
 
 # RHEL composes
@@ -250,14 +250,14 @@ export SRC_NETPERF="http://netqe-infra01.knqe.eng.rdu2.dc.redhat.com/share/tools
 
 # VM image names
 if [[ -z $VM_IMAGE ]]; then
-	export VM_IMAGE="rhel9.5.qcow2"
+	export VM_IMAGE="rhel9.4.qcow2"
 else
 	export VM_IMAGE=$VM_IMAGE
 fi
 
 # OVS packages
 if [[ -z $RPM_OVS ]]; then
-	export RPM_OVS=$OVS340_24F_RHEL9
+	export RPM_OVS=$OVS330_24F_RHEL9
 else
 	export RPM_OVS=$RPM_OVS
 fi
@@ -298,7 +298,7 @@ fi
 #export QEMU_KVM_RHEV_RHEL7=http://download.devel.redhat.com/brewroot/packages/qemu-kvm-rhev/2.12.0/48.el7_9.2/x86_64/qemu-kvm-rhev-2.12.0-48.el7_9.2.x86_64.rpm
 
 # OVN packages
-export RPM_OVN=$OVN340_24F_RHEL9 
+export RPM_OVN=$OVN330_24F_RHEL9 
 
 export BONDING_TESTS="ovs_test_bond_active_backup ovs_test_bond_set_active_slave ovs_test_bond_lacp_active ovs_test_bond_lacp_passive ovs_test_bond_balance_slb ovs_test_bond_balance_tcp"
 
@@ -351,11 +351,11 @@ pushd /home/ralongi/github/tools/ovs_testing
 ##./exec_topo.sh bmc57504 ovs_env=ovs-dpdk
 #./exec_topo.sh 6820c ovs_env=kernel
 ##./exec_topo.sh 6820c ovs_env=ovs-dpdk
-./exec_topo.sh mlx5_core bf3 ovs_env=kernel
-#./exec_topo.sh mlx5_core bf3 ovs_env=ovs-dpdk
+#./exec_topo.sh mlx5_core bf3 ovs_env=kernel
+##./exec_topo.sh mlx5_core bf3 ovs_env=ovs-dpdk
 
 #./exec_endurance.sh cx5
-#./exec_perf_ci.sh cx5
+./exec_perf_ci.sh cx5
 #./exec_endurance.sh cx6dx
 #./exec_perf_ci.sh cx6dx
 #./exec_endurance.sh cx6lx
