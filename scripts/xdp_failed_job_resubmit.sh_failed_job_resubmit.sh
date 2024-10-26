b43dda25ec9d0cb8ddfa436b76f743cc43455a60
Rick Alongi <ralongi@ralongi-thinkpadp16vgen1.rmtusma.csb>
2024-10-26 00:00:01 -0400

    Various changes

--- scripts/xdp_failed_job_resubmit.sh_failed_job_resubmit.sh
+++ scripts/xdp_failed_job_resubmit.sh_failed_job_resubmit.sh
@@ -11,8 +11,6 @@ if [[ $# -lt 1 ]]; then
 fi
 
 pushd ~
-
-task_name=$(bkr job-results J:$job_id --prettyxml | grep 'task name=' | grep -Ev 'distribution|crash' | head -1 | awk -F'"' '{print $2}')
 old_compose=$(bkr job-results J:$job_id --prettyxml | grep DISTRO_BUILD | head -1 | awk -F '"' '{print $4}')
 old_compose_ver=$(echo $old_compose | awk -F '-' '{print $2}' |  sed 's/.0//g')
 /home/ralongi/github/tools/scripts/get_kernel_from_compose.sh $old_compose_ver
@@ -22,11 +20,7 @@ if [[ $? -ne 0 ]]; then
 	#new_compose=$latest_compose_id
 fi
 
-if [[ $(echo $task_name | grep openvswitch) ]]; then
-	log=$(bkr job-results J:$job_id --prettyxml | grep -m 2 -A40 ${task_name} | grep taskout.log | awk -F '"' '{print $2}' | tail -1)
-elif [[ $(echo $task_name | grep ebpf_xdp) ]]; then
-	log=$(bkr job-results J:$job_id --prettyxml | grep -m 2 -A40 ${task_name} | grep taskout.log | awk -F '"' '{print $2}' | head -1)
-fi
+log=$(bkr job-results J:$job_id --prettyxml | grep -A40 '"/kernel/networking/openvswitch/topo" role="CLIENTS"' | grep taskout.log | awk -F '"' '{print $2}')
 result_file=$(basename $log)
 wget --quiet -O $result_file $log
 rm -f failed_tests.txt
@@ -39,43 +33,25 @@ elif [[ $(grep -i 'RESULT: FAIL' taskout.log) ]]; then # for when job aborted wi
 	for i in $(grep -i 'RESULT: FAIL' taskout.log | grep -v -i OVERALL | awk -F '(' '{print $NF}' | tr -d ')'); do
 		echo $i | sed 's/-/_/g' >> failed_tests.txt
 	done
-fi
-
-if [[ ! -s failed_tests.txt ]]; then
+else
 	echo "There are no failed tests reported in taskout.log"
-	popd
 	exit 0
 fi
 
-if [[ $(bkr job-clone J:$job_id --prettyxml --dryrun | grep -i 'openvswitch') ]]; then
-	original_ovs_topo=$(bkr job-clone J:$job_id --prettyxml --dryrun | grep '<param name="OVS_TOPO"' | awk -F '"' '{print $4}' | tail -1)
-	if [[ -z $new_ovs_topo ]]; then
-		new_ovs_topo=$(cat failed_tests.txt)
-		new_ovs_topo+='"'
-		new_ovs_topo='"'$new_ovs_topo
-		new_ovs_topo=$(echo $new_ovs_topo | dos2unix)
-		new_ovs_topo=$(echo $new_ovs_topo | tr -d '"')
-	fi
-elif [[ $(bkr job-clone J:$job_id --prettyxml --dryrun | grep -E -i 'xdp|bpf|bcc|l4lb') ]]; then
-	original_tests_to_run=$(bkr job-clone J:$job_id --prettyxml --dryrun | grep '<param name="TESTS_TO_RUN"' | awk -F '"' '{print $4}' | tail -1)
-	if [[ -z $new_tests_to_run ]]; then
-		new_tests_to_run=$(cat failed_tests.txt)
-		new_tests_to_run+='"'
-		new_tests_to_run='"'$new_tests_to_run
-		new_tests_to_run=$(echo $new_tests_to_run | dos2unix)
-		new_tests_to_run=$(echo $new_tests_to_run | tr -d '"')
-	fi
+# bkr job-clone J:$job_id --prettyxml --dryrun | grep -E -i 'xdp|bpf|bcc|l4lb'
+
+original_ovs_topo=$(bkr job-clone J:$job_id --prettyxml --dryrun | grep '<param name="OVS_TOPO"' | awk -F '"' '{print $4}' | tail -1)
+if [[ -z $new_ovs_topo ]]; then
+	new_ovs_topo=$(cat failed_tests.txt)
+	new_ovs_topo+='"'
+	new_ovs_topo='"'$new_ovs_topo
+	new_ovs_topo=$(echo $new_ovs_topo | dos2unix)
+	new_ovs_topo=$(echo $new_ovs_topo | tr -d '"')
 fi
 
 bkr job-clone J:$job_id --prettyxml --dryrun > new_job.xml
-if [[ $new_ovs_topo ]]; then
-	sed -i "s/$original_ovs_topo/$new_ovs_topo/g" new_job.xml
-elif [[ $new_tests_to_run ]]; then
-	sed -i "s/$original_tests_to_run/$new_tests_to_run/g" new_job.xml
-fi
-
+sed -i "s/$original_ovs_topo/$new_ovs_topo/g" new_job.xml
 if [[ $new_compose ]]; then sed -i "s/$old_compose/$new_compose/g" new_job.xml; fi
-
 bkr job-submit new_job.xml | tee new_job.log
 new_job_id=$(grep Submitted new_job.log | awk -F "'" {'print $2}')
 job_wb=$(bkr job-results $new_job_id | grep '<whiteboard>' | awk -F '<whiteboard>' '{print $2}' | awk -F '</whiteboard>' '{print $1}')
@@ -83,3 +59,10 @@ job_wb+=" \`Re-run of J:$job_id\`"
 bkr job-modify $new_job_id --whiteboard="$job_wb"
 rm -f /tmp/new_compose_id.txt
 popd
+
+
+
+	
+	
+
+
