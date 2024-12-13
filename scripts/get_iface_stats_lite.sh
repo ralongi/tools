8f729c575ee973bedcc0d23d175c154578088764
Rick Alongi <ralongi@ralongi-thinkpadp16vgen1.rmtusma.csb>
2024-12-13 00:00:01 -0500

    Changes

---
+++ scripts/get_iface_stats_lite.sh
@@ -0,0 +1,32 @@
+get_iface_stats_lite()
+{
+	iface=$1
+	suppress_stats_output=${suppress_stats_output:-"no"}
+	
+	start_tx_packets=$(cat /sys/class/net/$iface/statistics/tx_packets)	
+	start_rx_packets=$(cat /sys/class/net/$iface/statistics/rx_packets)	
+	sleep 3
+	end_tx_packets=$(cat /sys/class/net/$iface/statistics/tx_packets)
+	end_rx_packets=$(cat /sys/class/net/$iface/statistics/rx_packets)
+
+	total_tx_packets=$((($end_tx_packets - $start_tx_packets)))
+	total_rx_packets=$((($end_rx_packets - $start_rx_packets)))
+	echo "TX: $total_tx_packets packets observed on interface $iface."
+	echo "RX: $total_rx_packets packets observed on interface $iface."
+	
+	if [[ $suppress_stats_output != "yes" ]]; then
+		if [[ $total_tx_packets -le 0 ]]; then
+			echo "TX traffic was NOT observed on interface $iface."
+			echo "This may be expected if traffic is unidirectional.  Investigate further."
+		else
+			echo "TX traffic ($total_tx_packets packets) was observed on interface $iface."
+		fi
+		
+		if [[ $total_rx_packets -le 0 ]]; then
+			echo "RX traffic was NOT observed on interface $iface."
+			echo "This may be expected if traffic is unidirectional.  Investigate further."
+		else
+			echo "RX traffic ($total_rx_packets packets) was observed on interface $iface."
+		fi
+	fi
+}
