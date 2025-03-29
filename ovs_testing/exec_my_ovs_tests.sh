bb83e38b918b099d261877619633b545eb444309
Rick Alongi <ralongi@ralongi-thinkpadp16vgen1.rmtusma.csb>
2025-03-29 00:00:00 -0400

    Various changes

--- ovs_testing/exec_my_ovs_tests.sh
+++ ovs_testing/exec_my_ovs_tests.sh
@@ -339,9 +339,9 @@ pushd /home/ralongi/github/tools/ovs_testing
 #./exec_ovs_upgrade.sh
 #./exec_sanity_check.sh
 #./exec_ovs_qos.sh
-./exec_mcast_snoop.sh
+#./exec_mcast_snoop.sh
 #./exec_power_cycle_crash.sh
-#./exec_forward_bpdu.sh
+./exec_forward_bpdu.sh
 #./exec_of_rules.sh
 
 # To run just the ovs_test_ns_enable_nomlockall_CPUAffinity_test for topo, add "cpu" to the string of arguments
