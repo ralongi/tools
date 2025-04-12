79584ec79cd5ca3efafd3641aa30bd781c5c231f
Rick Alongi <ralongi@ralongi-thinkpadp16vgen1.rmtusma.csb>
2025-04-12 00:00:01 -0400

    Various changes

--- ovs_testing/exec_bcctools.sh
+++ ovs_testing/exec_bcctools.sh
@@ -10,9 +10,17 @@ echo "Compose: $COMPOSE"
 
 DBG_FLAG=${DBG_FLAG:-"set -x"}
 $DBG_FLAG
+XDP_LOAD_MODE=${XDP_LOAD_MODE:-"native"}
+XDP_TEST_FRAMEWORK=${XDP_TEST_FRAMEWORK:-"beakerlib"}
 SYSTYPE=${SYSTYPE:-"machine"}
+product="cpe:/o:redhat:enterprise_linux"
+retention_tag="active+1"
+NIC_NUM=2
+TEST_CONFIG=${TEST_CONFIG:-""}
+TESTS_TO_RUN=${TESTS_TO_RUN:-"all_tests"}
+GIT_HOME=~/git/my_fork
 
-pushd ~/temp
+pushd ~
 
 if [[ $driver == "ice" ]]; then
 	server="netqe51.knqe.eng.rdu2.dc.redhat.com"
@@ -93,9 +101,14 @@ elif [[ $driver == "atlantic" ]]; then
 fi
 
 if [[ $netscout_pair1 ]] || [[ $netscout_pair2 ]]; then
-	lstest ~/git/my_fork/kernel/networking/ebpf_xdp/bcctools | runtest --fetch-url kernel@https://gitlab.cee.redhat.com/kernel-qe/kernel/-/archive/master/kernel-master.tar.bz2 $COMPOSE --arch=x86_64 --topo=multiHost.1.1 --machine=$server,$client --systype=$SYSTYPE,$SYSTYPE $(echo "$zstream_repo_list") $(echo "$brew_build_cmd") --param=DBG_FLAG="$DBG_FLAG" --param=NAY=yes --param=NIC_NUM=2 --param=mh-NIC_DRIVER="${server_driver}","${client_driver}" $pciid_info --param=mh-TEST_DRIVER="${server_driver}","${client_driver}" --wb "(Server/DUT: $server, Client: $client), bcctools test, $COMPOSE, networking/ebpf_xdp/bcctools, Client driver: $client_driver, Server driver: $server_driver, Driver under test: $server_driver $special_info" --insert-task="/kernel/networking/openvswitch/netscout_connect_ports {dbg_flag=set -x} {netscout_pair1=$netscout_pair1} {netscout_pair2=$netscout_pair2}" --append-task="/kernel/networking/openvswitch/crash_check {dbg_flag=set -x}"
+	lstest $GIT_HOME/kernel/networking/ebpf_xdp/bcctools | runtest --fetch-url kernel@https://gitlab.cee.redhat.com/kernel-qe/kernel/-/archive/master/kernel-master.tar.bz2 $COMPOSE --product=$product --retention-tag=$retention_tag  --cmd-and-reboot="grubby --args=crashkernel=640M --update-kernel=ALL" --arch=x86_64 --topo=multiHost.1.1 --machine=$server,$client --systype=$SYSTYPE,$SYSTYPE $(echo "$zstream_repo_list") $(echo "$brew_build_cmd") --param=DBG_FLAG="$DBG_FLAG" --param=NAY=yes --param=NIC_NUM=2 --param=mh-NIC_DRIVER="${server_driver}","${client_driver}" $pciid_info --param=mh-TEST_DRIVER="${server_driver}","${client_driver}" --wb "(Server/DUT: $server, Client: $client), bcctools test, $COMPOSE, networking/ebpf_xdp/bcctools, Client driver: $client_driver, Server driver: $server_driver, Driver under test: $server_driver $special_info" --insert-task="/kernel/networking/openvswitch/netscout_connect_ports {dbg_flag=set -x} {netscout_pair1=$netscout_pair1} {netscout_pair2=$netscout_pair2}" --append-task="/kernel/networking/openvswitch/crash_check {dbg_flag=set -x}"
 else
-	lstest ~/git/my_fork/kernel/networking/ebpf_xdp/bcctools | runtest --fetch-url kernel@https://gitlab.cee.redhat.com/kernel-qe/kernel/-/archive/master/kernel-master.tar.bz2 $COMPOSE --arch=x86_64 --topo=multiHost.1.1 --machine=$server,$client --systype=$SYSTYPE,$SYSTYPE $(echo "$zstream_repo_list") $(echo "$brew_build_cmd") --param=DBG_FLAG="$DBG_FLAG" --param=NAY=yes --param=NIC_NUM=2 --param=mh-NIC_DRIVER="${server_driver}","${client_driver}" $pciid_info --param=mh-TEST_DRIVER="${server_driver}","${client_driver}" --wb "(Server/DUT: $server, Client: $client), bcctools test, $COMPOSE, networking/ebpf_xdp/bcctools, Client driver: $client_driver, Server driver: $server_driver, Driver under test: $server_driver $special_info" --append-task="/kernel/networking/openvswitch/crash_check {dbg_flag=set -x}"
+	lstest $GIT_HOME/kernel/networking/ebpf_xdp/bcctools | runtest --fetch-url kernel@https://gitlab.cee.redhat.com/kernel-qe/kernel/-/archive/master/kernel-master.tar.bz2 $COMPOSE --product=$product --retention-tag=$retention_tag  --cmd-and-reboot="grubby --args=crashkernel=640M --update-kernel=ALL" --arch=x86_64 --topo=multiHost.1.1 --machine=$server,$client --systype=$SYSTYPE,$SYSTYPE $(echo "$zstream_repo_list") $(echo "$brew_build_cmd") --param=DBG_FLAG="$DBG_FLAG" --param=NAY=yes --param=NIC_NUM=2 --param=mh-NIC_DRIVER="${server_driver}","${client_driver}" $pciid_info --param=mh-TEST_DRIVER="${server_driver}","${client_driver}" --wb "(Server/DUT: $server, Client: $client), bcctools test, $COMPOSE, networking/ebpf_xdp/bcctools, Client driver: $client_driver, Server driver: $server_driver, Driver under test: $server_driver $special_info" --append-task="/kernel/networking/openvswitch/crash_check {dbg_flag=set -x}" --append-task="/kernel/networking/openvswitch/crash_check {dbg_flag=set -x}"
+	
+# image mode
+
+	lstest $GIT_HOME/kernel/networking/ebpf_xdp/bcctools | runtest --fetch-url kernel@https://gitlab.cee.redhat.com/kernel-qe/kernel/-/archive/master/kernel-master.tar.bz2 $COMPOSE --product=$product --retention-tag=$retention_tag  --cmd-and-reboot="grubby --args=crashkernel=640M --update-kernel=ALL" --bootc="$COMPOSE",frompkg --packages="grubby nc pciutils wget driverctl iproute-tc wireshark llvm clang elfutils-libelf-devel libpcap-devel libbpf-devel lsof sysstat tcpdump iproute libibverbs bpftool xdp-tools" --arch=x86_64 --topo=multiHost.1.1 --machine=$server,$client --systype=$SYSTYPE,$SYSTYPE $(echo "$zstream_repo_list") $(echo "$brew_build_cmd") --param=DBG_FLAG="$DBG_FLAG" --param=NAY=yes --param=NIC_NUM=2 --param=mh-NIC_DRIVER="${server_driver}","${client_driver}" $pciid_info --param=mh-TEST_DRIVER="${server_driver}","${client_driver}" --wb "(Server/DUT: $server, Client: $client), bcctools test, $COMPOSE, networking/ebpf_xdp/bcctools, Client driver: $client_driver, Server driver: $server_driver, Driver under test: $server_driver $special_info" --append-task="/kernel/networking/openvswitch/crash_check {dbg_flag=set -x}"
+	
 fi
 
 popd 2>/dev/null
