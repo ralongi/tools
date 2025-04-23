8a46313859d2556f4beaf2a29429366c045601c9
Rick Alongi <ralongi@ralongi-thinkpadp16vgen1.rmtusma.csb>
2025-04-23 00:00:01 -0400

    Various changes

--- ovs_testing/exec_vm100.sh
+++ ovs_testing/exec_vm100.sh
@@ -4,23 +4,31 @@
 
 dbg_flag=${dbg_flag:-"set -x"}
 $dbg_flag
-dut="netqe05.knqe.eng.rdu2.dc.redhat.com"
 SYSTYPE=${SYSTYPE:-"machine"}
 ovs_rpm_name=$(echo $RPM_OVS | awk -F "/" '{print $NF}')
 product="cpe:/o:redhat:enterprise_linux"
 retention_tag="active+1"
 COMPOSE_VER=$(echo $COMPOSE | awk -F 'RHEL-' '{print $2}' | awk -F '.' '{print $1}')
 GIT_HOME=~/git/my_fork
-NAY=${NAY:-"yes"}
 arch=${arch:-"x86_64"}
 image_mode=${image_mode:-"no"}
+CLIENTS=helix93.telcoqe.eng.rdu2.dc.redhat.com
+SERVERS=helix94.telcoqe.eng.rdu2.dc.redhat.com
+NIC_DRIVER=mlx5_core
+VM_IMAGE=rhel9.6.qcow2
+NUMBER_OF_GUESTS=100
+PVT= no
+NAY=no
+GET_NIC_WITH_MAC=yes
+CLIENTS_NIC_MAC_STRING="9c:63:c0:25:fe:10"  #(for CLIENTS)
+SERVERS_NIC_MAC_STRING="5c:25:73:4a:0a:f2"  #(for SERVERS)
 
 pushd ~
 
 if [[ $image_mode == "yes" ]]; then
-	lstest $GIT_HOME/kernel/networking/openvswitch/vm100 | runtest --fetch-url kernel@https://gitlab.cee.redhat.com/kernel-qe/kernel/-/archive/master/kernel-master.tar.bz2 $COMPOSE --ks-append="rootpw redhat" --product=$product --retention-tag=$retention_tag --arch=$arch --topo=singlehost --machine=$dut --systype=$SYSTYPE $(echo "$zstream_repo_list") $(echo "$brew_build_cmd") $(echo "$cmds_to_run") --param=dbg_flag="$dbg_flag" --param=NAY=no --bootc=$COMPOSE,frompkg --packages="grubby sshpass iperf3 gcc gcc-c++ glibc-devel net-tools zlib-devel pciutils lsof tcl tk git wget nano driverctl dpdk dpdk-tools ipv6calc wireshark-cli nmap-ncat dnsmasq python3-scapy" --nrestraint --wb "(DUT: $dut), VM100 test, $COMPOSE, openvswitch/vm100 $special_info \`Image Mode\`" --insert-task="/kernel/networking/openvswitch/netscout_connect_ports {dbg_flag=set -x} {netscout_pair1=$netscout_pair1} {netscout_pair2=$netscout_pair2}"	
+	lstest $GIT_HOME/kernel/networking/openvswitch/vm100 | runtest --fetch-url kernel@https://gitlab.cee.redhat.com/kernel-qe/kernel/-/archive/master/kernel-master.tar.bz2 $COMPOSE --ks-append="rootpw redhat" --product=$product --retention-tag=$retention_tag --arch=$arch --topo=multiHost.1.1 --machine=$SERVERS,$CLIENTS --systype=$SYSTYPE $(echo "$zstream_repo_list") $(echo "$brew_build_cmd") $(echo "$cmds_to_run") --param=dbg_flag="$dbg_flag" --param=NAY=$NAY --param=PVT=$PVT --param=NIC_DRIVER=mlx5_core --param=VM_IMAGE=$VM_IMAGE --param=NUMBER_OF_GUESTS=$NUMBER_OF_GUESTS --param=GET_NIC_WITH_MAC=$GET_NIC_WITH_MAC --param=mh-NIC_MAC_STRING=$SERVERS_NIC_MAC_STRING,$CLIENTS_NIC_MAC_STRING --bootc=$COMPOSE,frompkg --packages="grubby sshpass iperf3 gcc gcc-c++ glibc-devel net-tools zlib-devel pciutils lsof tcl tk git wget nano driverctl dpdk dpdk-tools ipv6calc wireshark-cli nmap-ncat dnsmasq python3-scapy" --nrestraint --wb "(Server: $SERVERS, Client: $CLIENTS), VM100 test, $COMPOSE, openvswitch/vm100 $special_info \`Image Mode\`" --insert-task="/kernel/networking/openvswitch/netscout_connect_ports {dbg_flag=set -x} {netscout_pair1=$netscout_pair1} {netscout_pair2=$netscout_pair2}"	
 elif [[ $image_mode == "no" ]]; then
-	lstest $GIT_HOME/kernel/networking/openvswitch/vm100 | runtest --fetch-url kernel@https://gitlab.cee.redhat.com/kernel-qe/kernel/-/archive/master/kernel-master.tar.bz2 $COMPOSE --ks-append="rootpw redhat" --product=$product --retention-tag=$retention_tag --arch=$arch --topo=singlehost --machine=$dut --systype=$SYSTYPE $(echo "$zstream_repo_list") $(echo "$brew_build_cmd") $(echo "$cmds_to_run") --param=dbg_flag="$dbg_flag" --param=NAY=no --wb "(DUT: $dut), VM100 test, $COMPOSE, openvswitch/vm100 $special_info" --insert-task="/kernel/networking/openvswitch/netscout_connect_ports {dbg_flag=set -x} {netscout_pair1=$netscout_pair1} {netscout_pair2=$netscout_pair2}"	
+	lstest $GIT_HOME/kernel/networking/openvswitch/vm100 | runtest --fetch-url kernel@https://gitlab.cee.redhat.com/kernel-qe/kernel/-/archive/master/kernel-master.tar.bz2 $COMPOSE --ks-append="rootpw redhat" --product=$product --retention-tag=$retention_tag --arch=$arch --topo=multiHost.1.1 --machine=$SERVERS,$CLIENTS --systype=$SYSTYPE $(echo "$zstream_repo_list") $(echo "$brew_build_cmd") $(echo "$cmds_to_run") --param=dbg_flag="$dbg_flag" --param=NAY=$NAY --param=PVT=$PVT --param=NIC_DRIVER=mlx5_core --param=VM_IMAGE=$VM_IMAGE --param=NUMBER_OF_GUESTS=$NUMBER_OF_GUESTS --param=GET_NIC_WITH_MAC=$GET_NIC_WITH_MAC --param=mh-NIC_MAC_STRING=$SERVERS_NIC_MAC_STRING,$CLIENTS_NIC_MAC_STRING --nrestraint --wb "(Server: $SERVERS, Client: $CLIENTS), VM100 test, $COMPOSE, openvswitch/vm100 $special_info" --insert-task="/kernel/networking/openvswitch/netscout_connect_ports {dbg_flag=set -x} {netscout_pair1=$netscout_pair1} {netscout_pair2=$netscout_pair2}"	
 fi
 
 popd 2>/dev/null
--- scripts/create_errata.sh
+++ scripts/create_errata.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# Bash script to prep for using create-errata.py and to then launch create-errata.py using options provided to script
+# Must first clone git@gitlab.cee.redhat.com:nst/openvswitch/ovs-maintainer-scripts.git
+# This script keys off of what is set for GIT_HOME variable value and assumes above repo was cloned there
+# Make sure you have created ~/.env with the following contents (without the #):
+#ERRATA_OWNER_EMAIL=user@redhat.com
+#JIRA_OWNER_EMAIL=user@redhat.com
+#JIRA_TOKEN=XXX
+
 $dbg_flag
 
 FDP_RELEASE=$1
 NVR=$2
+GIT_HOME=~/git
 
 display_usage()
 {
@@ -17,14 +26,6 @@ then
     exit 0
 fi
 
-# Bash script to prep for using create-errata.py and to then launch create-errata.py using options provided to script
-# Must first clone git@gitlab.cee.redhat.com:nst/openvswitch/ovs-maintainer-scripts.git
-# This script assumes it was cloned under ~/git/
-# Make sure you have created ~/.env with the following contents (without the #):
-#ERRATA_OWNER_EMAIL=user@redhat.com
-#JIRA_OWNER_EMAIL=user@redhat.com
-#JIRA_TOKEN=XXX
-
 # Activate virtual python environment
 pushd ~
 python3 -m venv venv
@@ -34,7 +35,7 @@ pip install --upgrade pip
 pip install errata-tool
 
 # Make sure the latest create-errata.py is used
-pushd ~/git/ovs-maintainer-scripts/
+pushd $GIT_HOME/ovs-maintainer-scripts/
 git pull
 wait
 
@@ -42,6 +43,6 @@ wait
 /bin/cp -f create-errata.py ~
 popd
 
-create-errata.py $FD_RELEASE $NVR
+create-errata.py $FDP_RELEASE $NVR
 popd
 
