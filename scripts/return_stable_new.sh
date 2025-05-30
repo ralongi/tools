542f9013d242bfb5fe87417a0aa409efb34661e9
Rick Alongi <ralongi@ralongi-thinkpadp16vgen1.rmtusma.csb>
2025-05-30 00:00:00 -0400

    Various changes

--- scripts/return_stable_new.sh
+++ scripts/return_stable_new.sh
@@ -7,7 +7,9 @@ if [[ $machine ]]; then
 	ssh root@"$machine" return2beaker.sh
 else
 	rm -f ~/stable_systems.txt
-	bkr job-list --mine --w "Stable system" --w "tasks on" --unfinished > ~/stable_jobs.txt
+	bkr job-list --mine --w "Stable system" --unfinished  > ~/stable_jobs.txt
+	bkr job-list --mine --w "tasks on" --unfinished >> ~/stable_jobs.txt
+	bkr job-list --mine --w "Reserve RHEL" --unfinished  >> ~/stable_jobs.txt
 	job_list=$(cat ~/stable_jobs.txt | tr -d '[",]')
 	for i in $job_list; do 
 		system_name=$(bkr job-results "$i" --prettyxml | grep 'system value' | tail -1 | awk -F'"' '{print $2}')
