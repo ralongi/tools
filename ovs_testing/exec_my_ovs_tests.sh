d440decaef409021afea48c12bc56468b1df13ab
Rick Alongi <ralongi@ralongi-thinkpadp16vgen1.rmtusma.csb>
2025-06-03 00:00:00 -0400

    Various changes

--- ovs_testing/exec_my_ovs_tests.sh
+++ ovs_testing/exec_my_ovs_tests.sh
@@ -153,13 +153,13 @@ get_latest_driverctl()
 get_latest_driverctl
 
 if [[ -z $RPM_DRIVERCTL ]]; then
-	export RPM_DRIVERCTL=$DRIVERCTL_RHEL9
+	export RPM_DRIVERCTL=$DRIVERCTL_RHEL10
 fi
 if [[ -z $RPM_OVS_TCPDUMP_PYTHON ]]; then
-	export RPM_OVS_TCPDUMP_PYTHON=$OVS350_PYTHON_25C_RHEL9
+	export RPM_OVS_TCPDUMP_PYTHON=$OVS350_PYTHON_25C_RHEL10
 fi
 if [[ -z $RPM_OVS_TCPDUMP_TEST ]]; then
-	export RPM_OVS_TCPDUMP_TEST=$OVS350_TCPDUMP_25C_RHEL9
+	export RPM_OVS_TCPDUMP_TEST=$OVS350_TCPDUMP_25C_RHEL10
 fi
 
 # RHEL composes
@@ -270,14 +270,14 @@ export SRC_NETPERF="http://netqe-infra01.knqe.eng.rdu2.dc.redhat.com/share/tools
 
 # VM image names
 if [[ -z $VM_IMAGE ]]; then
-	export VM_IMAGE="rhel9.7.qcow2"
+	export VM_IMAGE="rhel10.1.qcow2"
 else
 	export VM_IMAGE=$VM_IMAGE
 fi
 
 # OVS packages
 if [[ -z $RPM_OVS ]]; then
-	export RPM_OVS=$OVS350_25C_RHEL9
+	export RPM_OVS=$OVS350_25C_RHEL10
 else
 	export RPM_OVS=$RPM_OVS
 fi
@@ -299,7 +299,7 @@ fi
 
 # SELinux packages
 if [[ -z $RPM_OVS_SELINUX_EXTRA_POLICY ]]; then
-	export RPM_OVS_SELINUX_EXTRA_POLICY=$OVS_SELINUX_25C_RHEL9
+	export RPM_OVS_SELINUX_EXTRA_POLICY=$OVS_SELINUX_25C_RHEL10
 else
 	export RPM_OVS_SELINUX_EXTRA_POLICY=$RPM_OVS_SELINUX_EXTRA_POLICY
 fi
@@ -318,7 +318,7 @@ fi
 #export QEMU_KVM_RHEV_RHEL7=http://download.devel.redhat.com/brewroot/packages/qemu-kvm-rhev/2.12.0/48.el7_9.2/x86_64/qemu-kvm-rhev-2.12.0-48.el7_9.2.x86_64.rpm
 
 # OVN packages
-export RPM_OVN=$OVN350_25C_RHEL9 
+export RPM_OVN=$OVN350_25C_RHEL10 
 
 export BONDING_TESTS="ovs_test_bond_active_backup ovs_test_bond_set_active_slave ovs_test_bond_lacp_active ovs_test_bond_lacp_passive ovs_test_bond_balance_slb ovs_test_bond_balance_tcp"
 
@@ -350,16 +350,16 @@ pushd /home/ralongi/github/tools/ovs_testing
 ##./exec_topo.sh ixgbe ovs_env=ovs-dpdk
 #./exec_topo.sh i40e ovs_env=kernel
 ##./exec_topo.sh i40e ovs_env=ovs-dpdk
-#./exec_topo.sh ice ovs_env=kernel
-##./exec_topo.sh ice ovs_env=ovs-dpdk
-#./exec_topo.sh ice_e830 ovs_env=kernel
-##./exec_topo.sh ice_e830 ovs_env=ovs-dpdk
-#./exec_topo.sh ice_e825 ovs_env=kernel
-##./exec_topo.sh ice_e825 ovs_env=ovs-dpdk
-#./exec_topo.sh mlx5_core cx5 ovs_env=kernel
-##./exec_topo.sh mlx5_core cx5 ovs_env=ovs-dpdk
-#./exec_topo.sh mlx5_core cx6 dx ovs_env=kernel
-##./exec_topo.sh mlx5_core cx6 dx ovs_env=ovs-dpdk
+./exec_topo.sh ice ovs_env=kernel
+#./exec_topo.sh ice ovs_env=ovs-dpdk
+./exec_topo.sh ice_e830 ovs_env=kernel
+#./exec_topo.sh ice_e830 ovs_env=ovs-dpdk
+./exec_topo.sh ice_e825 ovs_env=kernel
+#./exec_topo.sh ice_e825 ovs_env=ovs-dpdk
+./exec_topo.sh mlx5_core cx5 ovs_env=kernel
+#./exec_topo.sh mlx5_core cx5 ovs_env=ovs-dpdk
+./exec_topo.sh mlx5_core cx6 dx ovs_env=kernel
+#./exec_topo.sh mlx5_core cx6 dx ovs_env=ovs-dpdk
 #./exec_topo.sh mlx5_core cx6 lx ovs_env=kernel
 ##./exec_topo.sh mlx5_core cx6 lx ovs_env=ovs-dpdk
 #./exec_topo.sh arm ovs_env=kernel
@@ -404,7 +404,7 @@ pushd /home/ralongi/github/tools/ovs_testing
 #./exec_topo.sh nfp ovs_env=kernel
 ##./exec_topo.sh nfp ovs_env=ovs-dpdk
 
-./exec_ovs_memory_leak_soak.sh
+#./exec_ovs_memory_leak_soak.sh
 #./exec_ovn_memory_leak_soak.sh
 
 #./exec_regression_bug.sh
