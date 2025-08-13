7e016d1abfe73aacd93aed02e790700f7ebad721
Rick Alongi <ralongi@ralongi-thinkpadp16vgen1.rmtusma.csb>
2025-08-13 00:00:01 -0400

    Various changes

--- ovs_testing/exec_my_ovs_tests.sh
+++ ovs_testing/exec_my_ovs_tests.sh
@@ -160,10 +160,10 @@ if [[ -z $RPM_DRIVERCTL ]]; then
 	export RPM_DRIVERCTL=$DRIVERCTL_RHEL9
 fi
 if [[ -z $RPM_OVS_TCPDUMP_PYTHON ]]; then
-	export RPM_OVS_TCPDUMP_PYTHON=$OVS350_PYTHON_25D_RHEL9
+	export RPM_OVS_TCPDUMP_PYTHON=$OVS360_PYTHON_25D_RHEL9
 fi
 if [[ -z $RPM_OVS_TCPDUMP_TEST ]]; then
-	export RPM_OVS_TCPDUMP_TEST=$OVS350_TCPDUMP_25D_RHEL9
+	export RPM_OVS_TCPDUMP_TEST=$OVS360_TCPDUMP_25D_RHEL9
 fi
 
 # RHEL composes
@@ -274,14 +274,14 @@ export SRC_NETPERF="http://netqe-infra01.knqe.eng.rdu2.dc.redhat.com/share/tools
 
 # VM image names
 if [[ -z $VM_IMAGE ]]; then
-	export VM_IMAGE="rhel9.7.qcow2"
+	export VM_IMAGE="rhel9.6.qcow2"
 else
 	export VM_IMAGE=$VM_IMAGE
 fi
 
 # OVS packages
 if [[ -z $RPM_OVS ]]; then
-	export RPM_OVS=$OVS350_25D_RHEL9
+	export RPM_OVS=$OVS360_25D_RHEL9
 else
 	export RPM_OVS=$RPM_OVS
 fi
@@ -322,7 +322,7 @@ fi
 #export QEMU_KVM_RHEV_RHEL7=http://download.devel.redhat.com/brewroot/packages/qemu-kvm-rhev/2.12.0/48.el7_9.2/x86_64/qemu-kvm-rhev-2.12.0-48.el7_9.2.x86_64.rpm
 
 # OVN packages
-export RPM_OVN=$OVN350_25D_RHEL9 
+export RPM_OVN=$OVN360_25D_RHEL9 
 
 export BONDING_TESTS="ovs_test_bond_active_backup ovs_test_bond_set_active_slave ovs_test_bond_lacp_active ovs_test_bond_lacp_passive ovs_test_bond_balance_slb ovs_test_bond_balance_tcp"
 
@@ -377,8 +377,8 @@ pushd /home/ralongi/github/tools/ovs_testing
 #./exec_topo.sh mlx5_core_arm cx7 ovs_env=kernel
 
 ##./exec_topo.sh arm ovs_env=ovs-dpdk
-#./exec_topo.sh mlx5_core cx7 ovs_env=kernel
-##./exec_topo.sh mlx5_core cx7 ovs_env=ovs-dpdk
+./exec_topo.sh mlx5_core cx7 ovs_env=kernel
+#./exec_topo.sh mlx5_core cx7 ovs_env=ovs-dpdk
 #./exec_topo.sh mlx5_core bf2 ovs_env=kernel
 ##./exec_topo.sh mlx5_core bf2 ovs_env=ovs-dpdk
 #./exec_topo.sh sts ovs_env=kernel
@@ -414,7 +414,7 @@ pushd /home/ralongi/github/tools/ovs_testing
 #./exec_topo.sh nfp ovs_env=kernel
 ##./exec_topo.sh nfp ovs_env=ovs-dpdk
 
-./exec_ovs_memory_leak_soak.sh
+#./exec_ovs_memory_leak_soak.sh
 #./exec_ovn_memory_leak_soak.sh
 
 #./exec_regression_bug.sh
