759c3ff1507daa228d269f797ce1e394f65d2ced
Rick Alongi <ralongi@ralongi-thinkpadp16vgen1.rmtusma.csb>
2025-08-25 00:00:01 -0400

    Changes

--- ovs_testing/exec_my_ovs_tests.sh
+++ ovs_testing/exec_my_ovs_tests.sh
@@ -343,9 +343,9 @@ pushd /home/ralongi/github/tools/ovs_testing
 #./exec_ovs_upgrade.sh
 #./exec_sanity_check.sh
 #./exec_vm100.sh
-#./exec_ovs_qos.sh
+./exec_ovs_qos.sh
 #./exec_mcast_snoop.sh
-./exec_power_cycle_crash.sh
+#./exec_power_cycle_crash.sh
 #./exec_forward_bpdu.sh
 #./exec_of_rules.sh
 
